92098fe18286d81b2102be8e6b18b2ebc02f0315
Adriana Heesh <adrianaheesh@gmail.com>
2020-11-13 11:12:40 +1000

    updated seed file to create new instance of stores, and new instance of 1x product assigned to a store

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,5 @@
 class Store < ApplicationRecord
   belongs_to :user
-  has_many :products
+  has_many :products, dependent: :destroy
   has_one_attached :banner
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+Store.destroy_all
+Product.destroy_all
 
 users = [
     {
@@ -24,3 +26,85 @@ users = [
 ]
 
 User.create(users)
+
+stores = [
+    {
+        name: "I Love Plants",
+        description: "I love plants was created in 2020 with one goal: to bring life into your living space!",
+        user_id: User.find_by_email("iloveplants@plants.com").id
+    },
+    {
+        name: "Plant Posse",
+        description: "Your one-stop-shop for all things indoor plants.",
+        user_id: User.find_by_email("info@plantposse.com").id
+    }
+]
+
+Store.create(stores)
+
+products = [
+    {
+        name: "Mini cactus",
+        price: 15.00,
+        description: "The perfect desk companion due to its small size - watch out for the prickles though!",
+        availability: true,
+        store_id: Store.find_by_name("I Love Plants").id,
+        category: "Plant",
+        light: "Bright",
+        difficulty: "Plant Beginner"
+    }
+]
+    # ,
+    # {
+    #     name: ,
+    #     price: ,
+    #     description: ,
+    #     availability: ,
+    #     store: 2,
+    #     category: ,
+    #     light: ,
+    #     difficulty: 
+    # },
+    # {
+    #     name: ,
+    #     price: ,
+    #     description: ,
+    #     availability: ,
+    #     store: 2,
+    #     category: ,
+    #     light: ,
+    #     difficulty: 
+    # },
+    # {
+    #     name: ,
+    #     price: ,
+    #     description: ,
+    #     availability: ,
+    #     store: 1,
+    #     category: ,
+    #     light: ,
+    #     difficulty: 
+    # },
+    # {
+    #     name: ,
+    #     price: ,
+    #     description: ,
+    #     availability: ,
+    #     store: 1,
+    #     category: ,
+    #     light: ,
+    #     difficulty: 
+    # },
+    # {
+    #     name: ,
+    #     price: ,
+    #     description: ,
+    #     availability: ,
+    #     store: 1,
+    #     category: ,
+    #     light: ,
+    #     difficulty: 
+    # }
+
+
+Product.create(products)
