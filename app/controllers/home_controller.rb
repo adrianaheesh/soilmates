f29890f2d8db013c337bed063ec1000b099870e7
Adriana Heesh <adrianaheesh@gmail.com>
2020-11-19 14:04:21 +1000

    created queries in home controller and styled home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def page
+    @stores = Store.paginate(page: params[:page], per_page: 3)
+    @new_products = Product.order("created_at DESC").take(4)
+    @plants = Product.find_by_category("Plant")
+    @easy_plants = Product.find_by_difficulty("Plant beginner")
+    @medium_plants = Product.find_by_difficulty("Mildly smug") 
+    @difficult_plants = Product.find_by_difficulty("Full blown #plantfluencer")
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,32 +1,13 @@
 class OrdersController < ApplicationController
-  # def index
-  #   if @user_has_a_store
-  #     @orders = Order.where(store_id: current_user.store.id)
-  #   end
-  # end
-
   def success
     @order = Order.new(user_id: current_user.id, paid_status: true)
+    
     #assign product to the order for the view
     @order_product = Product.find(params[:id])
     @order.store_id = @order_product.store_id
     @order.save
+
     # update product so no longer available
     @order_product.update_attribute(:availability, false)
-    # @order_product.availability = false
-    # @order_product.save
   end
-
-  # def update
-  #   @order.update(order_params)
-  # end
-
-  # def show
-  #   @order.find(params[:id])
-  # end
-
-  # private
-  # def order_params
-  #   params.require(:order).permit(:store_id, :user_id, :paid_status, :completed)
-  # end
 end
